5785e9be50d17812e50bc8deedf9bac00a7d96fe
TokyoIncidents <tokyoincidents.g@gmail.com>
2012-05-23 16:02:30 +0900

    add create_symlink.sh(from https://github.com/sugyan/dotfiles)

---
+++ create_symlink.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+# https://raw.github.com/sugyan/dotfiles/master/create_symlink.sh
+cd $(dirname $0)
+for dotfile in .?*; do
+    case $dotfile in
+        *.elc)
+            continue;;
+        ..)
+            continue;;
+        .git)
+            continue;;
+        *)
+            ln -Fis "$PWD/$dotfile" $HOME
+            ;;
+    esac
+done
