97de04bd5b353223fa50e74d874a11218ca4387a
TokyoIncidents <tokyoincidents.g@gmail.com>
2012-06-07 14:42:31 +0900

    add windowmove

--- .emacs.d/inits/01_keybinding.el
+++ .emacs.d/inits/01_keybinding.el
@@ -15,3 +15,12 @@
 
 (global-set-key (kbd "C-x C-b")   nil)
 (global-set-key (kbd "C-x M-b")   'list-buffers)
+
+;; http://d.hatena.ne.jp/supermassiveblackhole/20101218/1292665209
+;; 反対側のウィンドウにいけるように
+(setq windmove-wrap-around t)
+;; C-M-{h,j,k,l}でウィンドウ間を移動
+(define-key global-map (kbd "C-M-k") 'windmove-up)
+(define-key global-map (kbd "C-M-j") 'windmove-down)
+(define-key global-map (kbd "C-M-l") 'windmove-right)
+(define-key global-map (kbd "C-M-h") 'windmove-left)
