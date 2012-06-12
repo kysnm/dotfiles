175b04f0354c1d0e1726fd33586770ebfb7cd937
TokyoIncidents <tokyoincidents.g@gmail.com>
2012-06-12 21:10:35 +0900

    add my-yas/prompt

--- .emacs.d/inits/40_yasnippet.el
+++ .emacs.d/inits/40_yasnippet.el
@@ -55,3 +55,19 @@
         try-expand-list try-expand-line
         try-complete-lisp-symbol-partially
         try-complete-lisp-symbol))
+
+;; http://d.hatena.ne.jp/sugyan/20120111/1326288445
+(defun my-yas/prompt (prompt choices &optional display-fn)
+  (let* ((names (loop for choice in choices
+                      collect (or (and display-fn (funcall display-fn choice))
+                                  coice)))
+         (selected (anything-other-buffer
+                    `(((name . ,(format "%s" prompt))
+                       (candidates . names)
+                       (action . (("Insert snippet" . (lambda (arg) arg))))))
+                    "*anything yas/prompt*")))
+    (if selected
+        (let ((n (position selected names :test 'equal)))
+          (nth n choices))
+      (signal 'quit "user quit!"))))
+(custom-set-variables '(yas/prompt-functions '(my-yas/prompt)))
