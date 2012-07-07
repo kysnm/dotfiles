1012188b47f999f92fed3df112a9f2c7a3d139e1
TokyoIncidents <tokyoincidents.g@gmail.com>
2012-07-07 17:10:55 +0900

    Move haskell settings one file. Remove submodule ghc-mod

--- .emacs.d/inits/30_auto-complete.el
+++ .emacs.d/inits/30_auto-complete.el
@@ -126,27 +126,3 @@
             (require 'auto-complete-ruby)
             (make-local-variable 'ac-omni-completion-sources)
             (setq ac-omni-completion-sources '(("\\.\\=" . (ac-source-rcodetools))))))
-
-;; for haskell-mode
-;; https://github.com/m2ym/auto-complete
-(ac-define-source ghc-mod
-  '((depends ghc)
-    (candidates . (ghc-select-completion-symbol))
-    (symbol . "s")
-    (cache)))
-
-(defun my-ac-haskell-mode ()
-  (setq ac-sources '(ac-source-words-in-same-mode-buffers ac-source-dictionary ac-source-ghc-mod)))
-(add-hook 'haskell-mode-hook 'my-ac-haskell-mode)
-
-(defun my-haskell-ac-init ()
-  (when (member (file-name-extension buffer-file-name) '("hs" "lhs"))
-    (auto-complete-mode t)
-    (setq ac-sources '(ac-source-words-in-same-mode-buffers ac-source-dictionary ac-source-ghc-mod))))
-
-(add-hook 'find-file-hook 'my-haskell-ac-init)
-
-;; auto-complete keybind
-(eval-after-load "key-chord"
-  '(progn
-     (key-chord-define-global "jk" 'auto-complete)))
--- .emacs.d/inits/50_haskell.el
+++ .emacs.d/inits/50_haskell.el
@@ -10,4 +10,63 @@
 
 ;; ghc-mod
 (autoload 'ghc-init "ghc" nil t)
+;; cabal でインストールしたライブラリのコマンドが格納されている bin ディレクトリへのパスを exec-path に追加する
+;; ghc-flymake.el などがあるディレクトリ ghc-mod を ~/.emacs.d 以下で管理することにした
+(if (eq system-type 'darwin)
+    (add-to-list 'exec-path (concat (getenv "HOME") "/Library/Haskell/bin/ghc-mod"))
+    (add-to-list 'load-path (concat (getenv "HOME") "/Library/Haskell/ghc-7.4.1/lib/ghc-mod-1.11.0/share/")))
 (add-hook 'haskell-mode-hook (lambda () (ghc-init) (flymake-mode)))
+
+(defvar anything-c-source-ghc-mod
+  '((name . "ghc-browse-document")
+    (init . anything-c-source-ghc-mod)
+    (candidates-in-buffer)
+    (candidate-number-limit . 9999999)
+    (action ("Open" . anything-c-source-ghc-mod-action))))
+
+(defun anything-c-source-ghc-mod ()
+  (unless (executable-find "ghc-mod")
+    (error "ghc-mod を利用できません。ターミナルで which したり、*scratch* で exec-path を確認したりしましょう"))
+  (let ((buffer (anything-candidate-buffer 'global)))
+    (with-current-buffer buffer
+      (call-process "ghc-mod" nil t t "list"))))
+
+(defun anything-c-source-ghc-mod-action (candidate)
+  (interactive "P")
+  (let* ((pkg (ghc-resolve-package-name candidate)))
+    (anything-aif (and pkg candidate)
+        (ghc-display-document pkg it nil)
+      (message "No document found"))))
+
+(defun anything-ghc-browse-document ()
+  (interactive)
+  (anything anything-c-source-ghc-mod))
+
+;; M-x anything-ghc-browse-document() に対応するキーの割り当て
+;; ghc-mod の設定のあとに書いた方がよいかもしれません
+(add-hook 'haskell-mode-hook
+  (lambda()
+    (define-key haskell-mode-map (kbd "C-M-d") 'anything-ghc-browse-document)))
+
+;; https://github.com/m2ym/auto-complete
+(ac-define-source ghc-mod
+  '((depends ghc)
+    (candidates . (ghc-select-completion-symbol))
+    (symbol . "s")
+    (cache)))
+
+(defun my-ac-haskell-mode ()
+  (setq ac-sources '(ac-source-words-in-same-mode-buffers ac-source-dictionary ac-source-ghc-mod)))
+(add-hook 'haskell-mode-hook 'my-ac-haskell-mode)
+
+(defun my-haskell-ac-init ()
+  (when (member (file-name-extension buffer-file-name) '("hs" "lhs"))
+    (auto-complete-mode t)
+    (setq ac-sources '(ac-source-words-in-same-mode-buffers ac-source-dictionary ac-source-ghc-mod))))
+
+(add-hook 'find-file-hook 'my-haskell-ac-init)
+
+;; auto-complete keybind
+(eval-after-load "key-chord"
+  '(progn
+     (key-chord-define-global "jk" 'auto-complete)))
