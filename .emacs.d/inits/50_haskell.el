f4507b4b8aa5c227d3cabe90e3612d04b92963ed
TokyoIncidents <tokyoincidents.g@gmail.com>
2012-07-07 14:27:33 +0900

    Add haskell-mode settings

---
+++ .emacs.d/inits/50_haskell.el
@@ -0,0 +1,13 @@
+(require 'haskell-mode)
+(require 'haskell-cabal)
+
+(add-to-list 'auto-mode-alist '("\\.hs$" . haskell-mode))
+(add-to-list 'auto-mode-alist '("\\.lhs$" . literate-haskell-mode))
+(add-to-list 'auto-mode-alist '("\\.cabal\\'" . haskell-cabal-mode))
+
+(add-to-list 'interpreter-mode-alist '("runghc" . haskell-mode))     ;#!/usr/bin/env runghc 用
+(add-to-list 'interpreter-mode-alist '("runhaskell" . haskell-mode)) ;#!/usr/bin/env runhaskell 用
+
+;; ghc-mod
+(autoload 'ghc-init "ghc" nil t)
+(add-hook 'haskell-mode-hook (lambda () (ghc-init) (flymake-mode)))
