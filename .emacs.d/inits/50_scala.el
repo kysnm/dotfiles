094b0697bd230f91573989b16b5572150f0ca293
TokyoIncidents <tokyoincidents.g@gmail.com>
2012-08-27 16:48:24 +0900

    Add my-scala-current-buffer

--- .emacs.d/inits/50_scala.el
+++ .emacs.d/inits/50_scala.el
@@ -1,1 +1,31 @@
+;; http://d.hatena.ne.jp/tototoshi/20100925/1285420294
 (require 'scala-mode-auto)
+
+(defvar my-scala-current-buffer nil)
+
+(defun my-scala-switch-to-interpreter ()
+  (interactive)
+  (setq my-scala-switch-to-interpreter (current-buffer))
+  (unless (scala-interpreter-running-p-1)
+	(scala-run-scala "scala"))
+  (scala-switch-to-interpreter))
+
+(defun my-scala-back-to-editing-buffer ()
+  (interactive)
+  (switch-to-buffer my-scala-current-buffer))
+
+(define-key scala-mode-map "\C-c\C-z" 'my-scala-switch-to-interpreter)
+(define-key scala-mode-inf-map "\C-c\C-z" 'my-scala-back-to-editing-buffer)
+
+(require 'scala-mode-feature-electric)
+    (add-hook 'scala-mode-hook
+	      (lambda ()
+		(scala-electric-mode)))
+
+(defadvice scala-eval-region (after pop-after-scala-eval-region)
+  (pop-to-buffer scala-inf-buffer-name))
+(ad-activate 'scala-eval-region)
+
+(defadvice scala-eval-buffer (after pop-after-scala-eval-buffer)
+  (pop-to-buffer scala-inf-buffer-name))
+(ad-activate 'scala-eval-buffer)
